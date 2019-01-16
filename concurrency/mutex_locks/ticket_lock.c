81819d8d7ce285e530a3f8c99ebe736f8663da62
Varad Gokhale <varad.gokhale91@gmail.com>
2019-01-16 05:17:00 -0600

    Create ticket_lock.c

---
+++ concurrency/mutex_locks/ticket_lock.c
@@ -0,0 +1,29 @@
+// advantage of this lock over other locks is that it guarantees that threads will be serviced on a first come first basis
+// no thread is starved
+
+typedef struct{
+  int ticket;
+  int turn;
+}mutex_t;
+
+void init(mutex_t* m){
+  m->ticket = 0;
+  m->turn = 0;
+}
+
+int fetch_and_add(int* turn)
+{
+  int ret = *turn;
+  *turn = ret + 1;
+  return ret;
+}
+
+void lock(mutex_t* m){
+  int myturn = fetch_and_add(&m->ticket);
+  while(myturn != m->turn)
+    ; //spin
+}
+
+void unlock(mutex_t* m){
+  m->turn += 1;
+}
